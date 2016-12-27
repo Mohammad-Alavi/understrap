177643b8c683c7c5ffc7ea034aa98b3bedbf76e2
Mohammad Alavi <mohammad.alavi1990@gmail.com>
2016-12-27 15:03:12 +0330

    Updated code for best practices

--- js/hide_search.js
+++ js/hide_search.js
@@ -2,7 +2,7 @@
 /* 
  * Toggles search on and off
  */
-jQuery(document).ready(function ($) {
+(function ($) {
     $(".search-toggle").click(function () {
         $("#search-container").slideToggle('fast', function () {
             $('.search-toggle').toggleClass('active');
@@ -10,4 +10,4 @@ jQuery(document).ready(function ($) {
         // Optional return false to avoid the page "jumping" when clicked
         return false;
     });
-});
+})(jQuery);
