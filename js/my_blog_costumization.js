a8a03e519ea716447b949878e10746be027249b1
Mohammad Alavi <mohammad.alavi1990@gmail.com>
2016-12-31 16:42:29 +0330

    Updated overall styles

--- js/my_blog_costumization.js
+++ js/my_blog_costumization.js
@@ -9,5 +9,12 @@
             $("#right-sidebar").removeClass("col-md-3").addClass("col-md-4");
             $("#primary").removeClass("col-md-7 offset-md-1").addClass("col-md-8");
         }
+
+        if (viewportWidth < 576) {
+            $(".navbar #main-menu > li a").not(".navbar #main-menu > li:last-of-type a").addClass("main-menu-toggle-only");
+        }
+        else {
+            $(".navbar #main-menu > li a").not(".navbar #main-menu > li:last-of-type a").removeClass("main-menu-toggle-only");
+        }
     });
 })(jQuery);
